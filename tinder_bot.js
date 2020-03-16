47c33b9a4c0b7622e8c7271947b6a5686e40da03
irshad <irshad.mike@gmail.com>
2020-03-17 00:34:23 +0530

    added the async promise version

--- tinder_bot.js
+++ tinder_bot.js
@@ -1,83 +1,52 @@
+const { Builder, By, Key, until, Capabilities } = require('selenium-webdriver');
+
+const loginDialog = By.css("div[role='dialog']");
+const phoneLoginButton = By.css("button[aria-label='Log in with phone number']");
+const newPhoneLoginButton = By.xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[2]/button');
+
+const moreOptions = By.xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/button');
 
 
-
-
-
-const tinderBot = async function () {
-
-    let chromeCapabilities = Capabilities.chrome();
+let chromeCapabilities = Capabilities.chrome();
     chromeCapabilities.set("goog:chromeOptions", {
         args: [
             "--lang=en",
             "disable-infobars"
         ]
       });
-    const driver = await new Builder()
+    const driver =  new Builder()
     .forBrowser('chrome')
     .withCapabilities(chromeCapabilities)
     .build();
+
+    driver.get('https://www.tinder.com')
+    .then(()=>{
+        driver.wait(until.elementsLocated(loginDialog), 60000);
+    })
+    .then(() => driver.sleep(4000))
+    .then(()=>{
+        console.log('clicking the login');
+        return driver.findElement(phoneLoginButton);
+    })
+    .then(
+        (element) => element.click(),
+        (error) =>{
+            console.log("login button was not found ");
+            driver.sleep(4000);
+            driver.findElement(moreOptions).click(); 
+            driver.sleep(2000);
+            driver.findElement(newPhoneLoginButton).click(); 
+        })
+     .then(()=> {
+         console.log("adding phone number");
+         driver.sleep(2000);
+         return driver.wait(until.elementsLocated(By.css("input[name='phone_number']")), 60000);
+        //  driver.findElement(By.css("input[name='phone_number']"))
+        // .sendKeys(99999999999);
+     }).then(webElements => {
+         console.log("webElements =>" ,webElements);
+         webElements[0].sendKeys(9999999999);
+     })
+    ;
+
     
-    await driver.get('https://www.tinder.com');
-
-    await driver.wait(until.elementsLocated(By.css("div[role='dialog']")), 60000);
-    await driver.sleep(4000);
-
-    try {
-        await (driver.findElement(By.css("button[aria-label='Log in with phone number']"))).click();
-    } catch (e) {
-        console.log("phone number not found");
-        
-        await driver.sleep(3000);
-        await driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/button')).click();
-        
-        await driver.sleep(3000);
-        await (driver.findElement(By.css("button[aria-label='Log in with phone number']"))).click();
-    }
-
-
-    await driver.wait(until.elementsLocated(By.css("input[name='phone_number']")), 50000);
-
-    await driver.findElement(By.css("input[name='phone_number']"))
-        .sendKeys(xxxx);
-
-
-    await driver.sleep(2000);
-
-    await driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/button')).click();
-
-    await driver.sleep(20000);
-
-    await driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/button')).click();
-
-    await driver.wait(until.elementsLocated(By.xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')), 20000);
-
-    await driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')).click();
-
-    await driver.sleep(4000);
-
-    await driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')).click();
-
-    await driver.sleep(2000);
-
-    const length = (await driver.getAllWindowHandles()).length;
-    console.log('length of windows = ', length);
-
-    // driver.switchTo()
-
-        while(true) {
-            await driver.sleep(1000);
-
-            await driver
-            .findElement(By.xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[2]/div[4]/button'))
-            .click();
-            await driver.sleep(1000);
-        }
-
-
-
-
-        console.log("y shit");
-    return driver;
-}
-
-tinderBot();
